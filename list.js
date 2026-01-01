ea2f29d4be95890af515c9c13151efa0a0c388db
todoom1 <aa13976383@outlook.com>
2026-01-02 01:49:19 +0800

    Add files via upload

---
+++ list.js
@@ -0,0 +1,45 @@
+document.getElementById("yh").addEventListener("click", function() {
+  window.location.href = "https://gfvgx.jgqvji.com/app/register.php?site_id=800&pt=D2D3CE32-E858-84B0-E29E-26A0ABCF1C4A";
+});
+document.getElementById("ff").addEventListener("click", function() {
+  window.location.href = "https://lncqc.jgqvji.com/app/register.php?site_id=1050&topId=4581763530274785&selfPlanId=1516364";
+});
+document.getElementById("kx").addEventListener("click", function() {
+  window.location.href = "https://onkrcy.fnbpsw.com/app/register.php?site_id=2080&pt=D6C29C91-D49C-85EC-50A5-C28542753918";
+});
+document.getElementById("df").addEventListener("click", function() {
+  window.location.href = "https://gfvgx.jgqvji.com/app/register.php?site_id=1030&topId=1371446";
+});
+document.getElementById("zt").addEventListener("click", function() {
+  window.location.href = "https://pkajim.hfjcl.com/app/register.php?site_id=1031&topId=34033s";
+});
+document.getElementById("cf").addEventListener("click", function() {
+  window.location.href = "https://pkajim.hfjcl.com/app/register.php?site_id=1032&topId=737920";
+});
+document.getElementById("t1").addEventListener("click", function() {
+  window.location.href = "https://lncqc.jgqvji.com/app/register.php?site_id=1051&topId=527419";
+});
+document.getElementById('st').addEventListener('click', function() {
+  window.location.href = "https://lejva.tprsmi.com/app/register.php?site_id=1058&topId=30500&selfPlanId=6125";
+});
+
+document.getElementById("0").innerHTML="🔥十年信誉平台推荐";
+
+document.getElementById("1").innerHTML="壹号娱乐";
+
+document.getElementById("2").innerHTML="凯旋娱乐";
+
+document.getElementById("3").innerHTML="超凡国际";
+
+document.getElementById("4").innerHTML="巅峰国际";
+
+document.getElementById("5").innerHTML="非凡娱乐";
+
+document.getElementById("6").innerHTML="征途国际";
+
+document.getElementById("9").innerHTML="Top1体育";
+
+document.getElementById("11").innerHTML="胜天国际";
+
+document.getElementById("57").innerHTML="P99A.MY";
+
