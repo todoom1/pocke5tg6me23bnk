665c76daa129fa41db2ae9bce80644a257b4a733
todoom1 <aa13976383@outlook.com>
2026-04-17 05:28:29 +0800

    Add files via upload

--- list.js
+++ list.js
@@ -1,69 +1,65 @@
-var yhz = "aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD04MDAmcHQ9RDJEM0NFMzItRTg1OC04NEIwLUUyOUUtMjZBMEFCQ0YxQzRB";
-var kxz = "aHR0cHM6Ly9vbmtyY3kuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MjA4MCZwdD1ENkMyOUM5MS1ENDlDLTg1RUMtNTBBNS1DMjg1NDI3NTM5MTg=";
-var cfz = "aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMyJnRvcElkPTczNzkyMA==";
-var dfz = "aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMwJnRvcElkPTEzNzE0NDY=";
-var ffz = "aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUwJnRvcElkPTQ1ODE3NjM1MzAyNzQ3ODUmc2VsZlBsYW5JZD0xNTE2MzY0";
-var ztz = "aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMxJnRvcElkPTM0MDMzcw==";
-var t1z = "aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUxJnRvcElkPTUyNzQxOQ==";
-var stz = "aHR0cHM6Ly9sZWp2YS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDU4JnRvcElkPTMwNTAwJnNlbGZQbGFuSWQ9NjEyNQ==";
+(function() {
+    var secretApps = [
+{img: './png/hygj.jpg', name: '汇赢国际', xurl: 'aHR0cHM6Ly9uY2djYS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDYyJnRvcElkPTEzNjIyJnNlbGZQbGFuSWQ9Njg3'},
+{img: './png/jlgj.jpg', name: '君临国际', xurl: 'aHR0cHM6Ly90ZmFqbi5oY2p3ai5jb20vYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTIwODEmdG9wSWQ9MjQwMTMmc2VsZlBsYW5JZD0zNjU='},
+{img: './png/yh.png', name: '壹号娱乐', xurl: 'aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD04MDAmcHQ9RDJEM0NFMzItRTg1OC04NEIwLUUyOUUtMjZBMEFCQ0YxQzRB'},
+{img: './png/stgj.jpg', name: '胜天国际', xurl: 'aHR0cHM6Ly9sZWp2YS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDU4JnRvcElkPTMwNTAwJnNlbGZQbGFuSWQ9NjEyNQ=='},
+{img: './png/kx.jpg', name: '凯旋娱乐', xurl: 'aHR0cHM6Ly9vbmtyY3kuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MjA4MCZwdD1ENkMyOUM5MS1ENDlDLTg1RUMtNTBBNS1DMjg1NDI3NTM5MTg='},
+{img: './png/cfgj.png', name: '超凡国际', xurl: 'aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMyJnRvcElkPTczNzkyMA=='},
+{img: './png/dfgj.png', name: '巅峰国际', xurl: 'aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMwJnRvcElkPTEzNzE0NDY='},
+{img: './png/ffyl.png', name: '非凡娱乐', xurl: 'aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUwJnRvcElkPTQ1ODE3NjM1MzAyNzQ3ODUmc2VsZlBsYW5JZD0xNTE2MzY0'},
+{img: './png/top1.png', name: 'Top1体育', xurl: 'aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUxJnRvcElkPTUyNzQxOQ=='},
+{img: './png/ztgj.png', name: '征途国际', xurl: 'aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMxJnRvcElkPTM0MDMzcw=='}
+    ];
 
-var yhx = atob(yhz);
-var kxx = atob(kxz);
-var cfx = atob(cfz);
-var dfx = atob(dfz);
-var ffx = atob(ffz);
-var ztx = atob(ztz);
-var t1x = atob(t1z);
-var stx = atob(stz);
+    var container = document.getElementById('xapes');
+    if (!container) return;
 
-document.getElementById("yh").addEventListener("click", function() {
-window.open(yhx, '_blank');
-});
-document.getElementById("ff").addEventListener("click", function() {
-window.open(ffx, '_blank');
-});
-document.getElementById("kx").addEventListener("click", function() {
-window.open(kxx, '_blank');
-});
-document.getElementById("df").addEventListener("click", function() {
-window.open(dfx, '_blank');
-});
-document.getElementById("zt").addEventListener("click", function() {
-window.open(ztx, '_blank');
-});
-document.getElementById("cf").addEventListener("click", function() {
-window.open(cfx, '_blank');
-});
-document.getElementById("t1").addEventListener("click", function() {
-window.open(t1x, '_blank');
-});
-document.getElementById('st').addEventListener('click', function() {
-window.open(stx, '_blank');
-});
-document.getElementById("jl").addEventListener("click", function() {
-window.open(atob("aHR0cHM6Ly90ZmFqbi5oY2p3ai5jb20vYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTIwODEmdG9wSWQ9MjQwMTMmc2VsZlBsYW5JZD0zNjU="), '_blank');
-});
+    secretApps.forEach(function(app) {
+        // 1. 创建元素
+        var li = document.createElement('li');
+        li.className = 'app-item'; // 只保留类名用于样式
 
-;window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']("\x32")['\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c']="\ud83d\udd25\u5341\u5e74\u4fe1\u8a89\u5e73\u53f0\u63a8\u8350";window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']("\x79\x68\x6e")['\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c']="\u58f9\u53f7\u5a31\u4e50";window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']("\x6b\x78\x6e")['\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c']="\u51ef\u65cb\u5a31\u4e50";window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']("\x63\x66\x6e")['\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c']="\u8d85\u51e1\u56fd\u9645";window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']("\x64\x66\x6e")['\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c']="\u5dc5\u5cf0\u56fd\u9645";window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']("\x66\x66\x6e")['\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c']="\u975e\u51e1\u5a31\u4e50";window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']("\x7a\x74\x6e")['\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c']="\u5f81\u9014\u56fd\u9645";window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']("\x74\x31\x6e")['\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c']="\x54\x6f\x70\x31\u4f53\u80b2";window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']("\x73\x74\x6e")['\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c']="\u80dc\u5929\u56fd\u9645";window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]['\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74\x42\x79\x49\x64']("\x6a\x6c\x6e")['\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c']="\u541b\u4e34\u56fd\u9645";
-window["document"]['getElementById']("hy")['addEventListener']("click", function () {
-    window['open'](atob("aHR0cHM6Ly9uY2djYS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDYyJnRvcElkPTEzNjIyJnNlbGZQbGFuSWQ9Njg3"), '_blank')
+        li.innerHTML = `
+            <div class="app-icon"><img src="${app.img}"></div>
+            <div class="app-name">${app.name}</div>
+        `;
+
+        li.onclick = function() {
+            try {
+                // 解码并跳转
+                var targetUrl = atob(app.xurl.trim());
+                window.open(targetUrl, '_blank');
+            } catch (e) {
+                console.error("解码失败:", e);
+            }
+        };
+
+        // 4. 塞进容器
+        container.appendChild(li);
+    });
+})();
+
+window["document"]['getElementById']("3")['innerHTML'] = ("2026世界杯<br>点击查看赛程");
+window["document"]['getElementById']("2")['innerHTML'] = ("🔥十年信誉平台<br>⚽世界杯官方投注平台");
+window["document"]['getElementById']("1")['innerHTML'] = "P99A.MY";
+
+
+function showPopupOnce(popupId = 'sw_loaded') {
+  const key = `popup_${popupId}_shown`;
+  
+  if (localStorage.getItem(key)) {
+    return; 
+  }
+  alert('欢迎访问P99A！');
+  localStorage.setItem(key, Date.now().toString());
+}
+window.addEventListener('load', function() {
+  showPopupOnce();
 });
-window["document"]['getElementById']("hyn")['innerHTML'] = ("汇赢国际");
-document.getElementById("1").innerHTML="P99A.MY";
-const texts = ["祝您新年快乐！", "马到成功", "P99A.MY"];
-const today = new Date();
-const month = today.getMonth() + 1;
-const year = today.getFullYear();
-const date = today.getDate();
-let currentIndex = 0;
-
-if (year === 2026 && month === 2 && date >= 17 && date <= 23) {
-    alert("🎇祝您新年快乐！🎇"); 
-setInterval(() => {
-document.getElementById("1").innerHTML = texts[currentIndex];
-currentIndex = (currentIndex + 1) % texts.length;
-}, 2026);
-
-} else if (year === 2026 && month === 3 && date === 3) {
-    alert("🎇祝您元宵节快乐！🎇");
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(function(registration) {
+      console.log('SW registered');
+    });
 }
