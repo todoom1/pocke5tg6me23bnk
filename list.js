92b5fc82c10f5a2e91e50c0b55d12651d2661f6b
todoom1 <aa13976383@outlook.com>
2026-05-31 12:05:52 +0800

    Update list.js

--- list.js
+++ list.js
@@ -1,11 +1,12 @@
 (function() {
     var secretApps = [
+{img: './png/xjh.jpg', name: '馨聚汇', xurl: 'aHR0cHM6Ly95Ymt2YnoudGNrcXh4LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTA2NiZ0b3BJZD0yNzIwMSZzZWxmUGxhbklkPTc1MA=='},
 {img: './png/lhgj.jpg', name: '领航国际', xurl: 'aHR0cHM6Ly9yc2VzcmEuaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDY0JnB0PTI3MUJBNUIwLTM0RTMtM0NEQi03MEQyLTVDRTFCMDc0NTRBRg=='},
 {img: './png/hygj.jpg', name: '汇赢国际', xurl: 'aHR0cHM6Ly9uY2djYS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDYyJnRvcElkPTEzNjIyJnNlbGZQbGFuSWQ9Njg3'},
 {img: './png/jlgj.jpg', name: '君临国际', xurl: 'aHR0cHM6Ly90ZmFqbi5oY2p3ai5jb20vYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTIwODEmdG9wSWQ9MjQwMTMmc2VsZlBsYW5JZD0zNjU='},
 {img: './png/yh.png', name: '壹号娱乐', xurl: 'aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD04MDAmcHQ9RDJEM0NFMzItRTg1OC04NEIwLUUyOUUtMjZBMEFCQ0YxQzRB'},
 {img: './png/stgj.jpg', name: '胜天国际', xurl: 'aHR0cHM6Ly9sZWp2YS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDU4JnRvcElkPTMwNTAwJnNlbGZQbGFuSWQ9NjEyNQ=='},
-{img: './png/kx.jpg', name: '凯旋娱乐', xurl: 'aHR0cHM6Ly9vbmtyY3kuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MjA4MCZwdD1ENkMyOUM5MS1ENDlDLTg1RUMtNTBBNS1DMjg1NDI3NTM5MTg='},
+//{img: './png/kx.jpg', name: '凯旋娱乐', xurl: 'aHR0cHM6Ly9vbmtyY3kuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MjA4MCZwdD1ENkMyOUM5MS1ENDlDLTg1RUMtNTBBNS1DMjg1NDI3NTM5MTg='},
 {img: './png/cfgj.png', name: '超凡国际', xurl: 'aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMyJnRvcElkPTczNzkyMA=='},
 {img: './png/dfgj.png', name: '巅峰国际', xurl: 'aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMwJnRvcElkPTEzNzE0NDY='},
 {img: './png/ffyl.png', name: '非凡娱乐', xurl: 'aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUwJnRvcElkPTQ1ODE3NjM1MzAyNzQ3ODUmc2VsZlBsYW5JZD0xNTE2MzY0'},
